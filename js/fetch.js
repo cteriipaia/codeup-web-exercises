b4cf969b22af4c2b113eb242bcd86d0449e4a2c3
Chanee Teriipaia-Molett <chanee.teriipaiamolett@gmail.com>
2023-11-30 10:18:08 -0600

    feat:clean up fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -3,14 +3,14 @@
     console.log(GH_KEY)
 
     function getLastCommitDate(username, key){
-        fetch('https://api.github.com/users/${username}/events/public', {headers: {'Authorization': `token ${key}`}}).then(resp => resp.json()).then(events => {
+        fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `token ${key}`}}).then(resp => resp.json()).then(events => {
             for (let event of events) {
                 if (event.type === "PushEvent") {
             console.log (event.created_at);
             return event.payload.commits [0].url
                 }
             }
-        }).then (url => fetch(url,{headers:{'Authorization': `token ${key}`}})).then(resp =>resp.json()).then (data=> {
+        }).then (url => fetch(url,{headers:{'Authorization': `token ${key}`}})).then(resp =>resp.json()).then (data => {
             console.log(new Date(data.commit.author.date));
         });
     }
