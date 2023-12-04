db2ff174ffc43f4c5641c0d40ccde7224c825e05
Chanee Teriipaia-Molett <chanee.teriipaiamolett@gmail.com>
2023-12-04 15:31:18 -0600

    feat:finally got the code to work.

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,85 +1,85 @@
 "use strict";
-// add effie
-    (() => {
-// Replace with your OpenWeatherMap API key
-        const weatherApiKey = 'YOUR_OPENWEATHERMAP_API_KEY';
-// Replace with your Mapbox API key
-        const mapboxApiKey = MB_KEY;
+// add location
+    const lat = 29.426825118534886;
+    const lon = -98.48948239256946;
+    let marker;
+// Initialize Mapbox map
+    mapboxgl.accessToken = MB_KEY;
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [lon, lat],
+        zoom: 8
+    });
+    //Adds the zoom control to the map
+    map.addControl(new mapboxgl.NavigationControl());
+    // Event Listener for the select zoom option
+    document.getElementById('zoom-level').addEventListener('change', function (event) {
+        const zoomLevel = parseFloat(event.target.value);
+        map.setZoom(zoomLevel);
+    });
+        fetch(`https://api.openweathermap.org/data/2.5/weather?` +
+            `lat=29.426825118534886&lon=-98.48948239256946` +
+            `&appid=${WM_KEY}`)
+            .then( data => console.log(data));
 
-// Function to create forecast cards
-        function createForecastCards(coordinates) {
-            // Implement the logic to create forecast cards based on the provided coordinates
-            // You can use the coordinates to fetch weather data and update the UI
-            fetchWeatherData(coordinates.lat, coordinates.lng);
-        }
-
-// Function to get weather data based on coordinates
-        async function getWeatherData(lat, lon) {
-            const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
-            const response = await fetch(weatherUrl);
-            const data = await response.json();
-            return data;
-        }
-
-// Function to update the weather forecast UI
-        function updateForecastUI(data) {
-            // Implement the code to update the forecast UI with the received data
-            // You can access specific weather information from the 'data' object
-            console.log(data);
-        }
-
-// Function to fetch weather data based on coordinates
-        function fetchWeatherData(lat, lon) {
-            // Update the API endpoint with the correct format
-            const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
-
-            fetch(weatherUrl)
-                .then(response => response.json())
-                .then(data => {
-                    // Process the data and update the forecast UI
-                    updateForecastUI(data);
-                })
-                .catch(error => {
-                    console.error('Error fetching weather data:', error);
-                });
-        }
-
-// Function to update the forecast when the map is clicked
-        function updateForecast(lat, lon) {
-            fetchWeatherData(lat, lon);
-        }
-
-// Initialize Mapbox
-        mapboxgl.accessToken = mapboxApiKey;
-        const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [-98.48948239256946, 29.426825118534886], // Default center (longitude, latitude)
-            zoom: 9
+// Add draggable marker
+    marker = new mapboxgl.Marker({
+        draggable: true
+    }).setLngLat([lon, lat])
+        .addTo(map);
+// Create the forecast cards for the original location, calling the createForecastCards function
+createForecastCards(marker.getLngLat());
+//When the marker is dragged, update the forecast cards
+    marker.on('dragend', function () {
+        createForecastCards(marker.getLngLat());
+    });
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WM_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        createForcastCards(lon, lat);
+    })
+        .catch(error => console.log(error));
+// For the user to search an address and place a marker
+document.getElementById('searchButton').addEventListener('click', function () {
+    const userInput = document.getElementById('search').value;
+    geocode(userInput, MB_KEY).then(function (result) {
+        map.flyTo({ //Moves the center of the map to markers new location
+            center: result,
+            essential: true
         });
-
-// Event listener for when the map is clicked
-        map.on('click', (e) => {
-            const { lng, lat } = e.lngLat;
-            // Update the forecast when the map is clicked
-            updateForecast(lat, lng);
+        marker.setLngLat(result);
+//Catching the error if the user inputs an invalid address
+    }).catch(function (error) {
+        console.error(error);
+        alert('Invalid address. Please try again.');
+    });
+});
+// Create cards for the forecast based on the location of the marker
+function createForecastCards(lngLat) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lngLat.lat}&lon=${lngLat.lng}&appid=${WM_KEY}&units=imperial`)
+        .then(data => data.json())
+        .then(forecast => {
+// Create HTML structure for the forecast
+            let forecastHTML = '';
+            for (let i = 0; i < forecast.list.length; i += 8) {
+                const day = forecast.list[i];
+                const date = new Date(day.dt * 1000).toDateString();
+                const addIcon = day.weather[0].icon;
+                const iconUrl = `https://openweathermap.org/img/wn/${addIcon}@2x.png`; // Pulled this link from the open weather map api
+                forecastHTML += `
+                    <div class="forecast-card">
+                        <h3>${date}</h3>
+                        <img src=${iconUrl} alt=${day.weather[0].description} />
+                        <p>Description: ${day.weather[0].description}</p>
+                        <p>Temperature: ${day.main.temp}°F</p>
+                        <p>Humidity: ${day.main.humidity}%</p>
+                        <p>Pressure: ${day.main.pressure} hPa</p> 
+                    </div>
+                `;
+            }
+// Add the forecast cards to the page
+            document.getElementById('weatherInfoCard').innerHTML += forecastHTML;
         });
-
-// Add Mapbox Geocoder for location search
-        const geocoder = new MapboxGeocoder({
-            accessToken: mapboxApiKey,
-            mapboxgl: mapboxgl
-        });
-
-// Add the geocoder to your page
-        document.getElementById('map').appendChild(geocoder.onAdd(map));
-
-// Optional: You may want to initialize the forecast with your current location
-        navigator.geolocation.getCurrentPosition(position => {
-            const { latitude, longitude } = position.coords;
-            // Update the forecast for the initial location
-            createForecastCards({ lat: latitude, lng: longitude });
-        });
-
-    })();
-
+}
