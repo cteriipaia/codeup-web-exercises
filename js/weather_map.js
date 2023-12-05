f03df6d0e3cead3252e390b0e4ab5abfb8a925b5
Chanee Teriipaia-Molett <chanee.teriipaiamolett@gmail.com>
2023-12-05 14:38:24 -0600

    feat:completed and presented weather map.

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,16 +1,19 @@
 "use strict";
-// add location
-    const lat = 29.426825118534886;
-    const lon = -98.48948239256946;
+    // add location
+    let lat = 0;
+    let lon = 0;
+    const codetteMarkerImage = 'img/codette.png';
     let marker;
-// Initialize Mapbox map
+
+    // Initialize Mapbox map
     mapboxgl.accessToken = MB_KEY;
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v12',
-        center: [lon, lat],
+        center: [-98.48948239256946, 29.426825118534886],
         zoom: 8
     });
+
     //Adds the zoom control to the map
     map.addControl(new mapboxgl.NavigationControl());
     // Event Listener for the select zoom option
@@ -23,63 +26,82 @@
             `&appid=${WM_KEY}`)
             .then( data => console.log(data));
 
-// Add draggable marker
+    // Add draggable marker
     marker = new mapboxgl.Marker({
-        draggable: true
-    }).setLngLat([lon, lat])
+        draggable: true,
+        color: 'red',
+        element: createCustomMarker(codetteMarkerImage),
+    }).setLngLat([-98.48948239256946, 29.426825118534886])
         .addTo(map);
-// Create the forecast cards for the original location, calling the createForecastCards function
-createForecastCards(marker.getLngLat());
-//When the marker is dragged, update the forecast cards
-    marker.on('dragend', function () {
-        createForecastCards(marker.getLngLat());
-    });
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WM_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        createForcastCards(lon, lat);
-    })
-        .catch(error => console.log(error));
-// For the user to search an address and place a marker
-document.getElementById('searchButton').addEventListener('click', function () {
-    const userInput = document.getElementById('search').value;
-    geocode(userInput, MB_KEY).then(function (result) {
-        map.flyTo({ //Moves the center of the map to markers new location
-            center: result,
-            essential: true
+
+    // Create a custom marker using the custom image
+    function createCustomMarker(imageUrl) {
+        const img = document.createElement('img');
+        img.src = imageUrl;
+        img.style.width = '30px';
+        img.style.height = '30px';
+        img.style.borderRadius = '50%';
+        img.style.boxShadow = '0 0 10px rgba(0,0,0,0.5)';
+
+        return img;
+    }
+
+    // Create the forecast cards for the original location, calling the createForecastCards function
+    createForecastCards(marker.getLngLat());
+    //When the marker is dragged, update the forecast cards
+        marker.on('dragend', function () {
+            createForecastCards(marker.getLngLat());
+            console.log(marker.getLngLat());
         });
-        marker.setLngLat(result);
-//Catching the error if the user inputs an invalid address
-    }).catch(function (error) {
-        console.error(error);
-        alert('Invalid address. Please try again.');
-    });
-});
-// Create cards for the forecast based on the location of the marker
-function createForecastCards(lngLat) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lngLat.lat}&lon=${lngLat.lng}&appid=${WM_KEY}&units=imperial`)
-        .then(data => data.json())
-        .then(forecast => {
-// Create HTML structure for the forecast
-            let forecastHTML = '';
-            for (let i = 0; i < forecast.list.length; i += 8) {
-                const day = forecast.list[i];
-                const date = new Date(day.dt * 1000).toDateString();
-                const addIcon = day.weather[0].icon;
-                const iconUrl = `https://openweathermap.org/img/wn/${addIcon}@2x.png`; // Pulled this link from the open weather map api
-                forecastHTML += `
-                    <div class="forecast-card">
-                        <h3>${date}</h3>
-                        <img src=${iconUrl} alt=${day.weather[0].description} />
-                        <p>Description: ${day.weather[0].description}</p>
-                        <p>Temperature: ${day.main.temp}°F</p>
-                        <p>Humidity: ${day.main.humidity}%</p>
-                        <p>Pressure: ${day.main.pressure} hPa</p> 
-                    </div>
-                `;
-            }
-// Add the forecast cards to the page
-            document.getElementById('weatherInfoCard').innerHTML += forecastHTML;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WM_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            createForcastCards(lon, lat);
+        })
+            .catch(error => console.log(error));
+
+    // For the user to search an address and place a marker
+    document.getElementById('searchButton').addEventListener('click', function () {
+        const userInput = document.getElementById('search').value;
+        geocode(userInput, MB_KEY).then(function (result) {
+            map.flyTo({ //Moves the center of the map to markers new location
+                center: result,
+                essential: true
+            });
+            marker.setLngLat(result);
+            createForecastCards(marker.getLngLat());
+        //Catching the error if the user inputs an invalid address
+        }).catch(function (error) {
+            console.error(error);
+            alert('Invalid address. Please try again.');
         });
-}
+    });
+
+    // Create cards for the forecast based on the location of the marker
+    function createForecastCards(lngLat) {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lngLat.lat}&lon=${lngLat.lng}&appid=${WM_KEY}&units=imperial`)
+            .then(data => data.json())
+            .then(forecast => {
+                const weatherInfoCard = document.getElementById('weatherInfoCard');
+                weatherInfoCard.innerHTML = '';
+                let forecastHTML = '';
+                for (let i = 0; i < forecast.list.length; i += 8) {
+                    const day = forecast.list[i];
+                    const date = new Date(day.dt * 1000).toDateString();
+                    const addIcon = day.weather[0].icon;
+                    const iconUrl = `https://openweathermap.org/img/wn/${addIcon}@2x.png`;
+                    forecastHTML += `
+                        <div class="forecast-card">
+                            <h3>${date}</h3>
+                            <img src=${iconUrl} alt=${day.weather[0].description} />
+                            <p>Description: ${day.weather[0].description}</p>
+                            <p>Temperature: ${day.main.temp}°F</p>
+                            <p>Humidity: ${day.main.humidity}%</p>
+                            <p>Pressure: ${day.main.pressure} hPa</p> 
+                        </div>
+                    `;
+                }
+                document.getElementById('weatherInfoCard').innerHTML += forecastHTML;
+            });
+    }
