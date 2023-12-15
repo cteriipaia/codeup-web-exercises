ed5af66b72da9e2fc7eb85115d0f0db93deb79fc
Chanee Teriipaia-Molett <chanee.teriipaiamolett@gmail.com>
2023-12-14 20:13:49 -0600

    feat: clean up and finish uploading ajax-store.html/inventory.json.

--- js/fetch.js
+++ js/fetch.js
@@ -6,8 +6,8 @@
         fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `token ${key}`}}).then(resp => resp.json()).then(events => {
             for (let event of events) {
                 if (event.type === "PushEvent") {
-            console.log (event.created_at);
-            return event.payload.commits [0].url
+                    console.log (event.created_at);
+                    return event.payload.commits [0].url
                 }
             }
         }).then (url => fetch(url,{headers:{'Authorization': `token ${key}`}})).then(resp =>resp.json()).then (data => {
